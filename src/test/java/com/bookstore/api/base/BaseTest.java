908bb3fa33094a490332f123808d950af0d1ec6e
sourabhgautam26 <154259893+sourabhgautam26@users.noreply.github.com>
2025-09-12 16:35:04 +0530

    first commit

---
+++ src/test/java/com/bookstore/api/base/BaseTest.java
@@ -0,0 +1,22 @@
+package com.bookstore.api.base;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void setup() {
+        // Base URI from Endpoints
+        RestAssured.baseURI = "http://localhost:8080/api";
+
+        // Enable logging for requests and responses
+        RestAssured.filters(new RequestLoggingFilter(LogDetail.ALL), new ResponseLoggingFilter(LogDetail.ALL));
+
+        // Default content type
+        RestAssured.useRelaxedHTTPSValidation();  // handles SSL if needed
+    }
+}
